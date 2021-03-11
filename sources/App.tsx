dad2d30ed975ac40316ef1024f1253df8035ce99
a-nice-username <rynvva@gmail.com>
2021-03-11 07:12:26 +0700

    :sparkles: add vscode rule, editor tab size to 2

--- sources/App.tsx
+++ sources/App.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 
 import MessageListener from './helpers/messaging-and-notification'
-
 import StackNavigator from './navigators/stack-navigator'
 
 function App() {
